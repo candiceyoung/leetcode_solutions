af39ec387c3b5fdbd2c35a3e44036fb14a73f36f
Candice Yang <capricorn.young@gmail.com>
2015-11-10 22:03:06 -0800

    minimum_depth_of_binary_tree

---
+++ minimum_depth_of_binary_tree.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        int i = 0;
+        while (!q.empty()) {
+            i++;
+            int size = q.size();
+            for (int j = 0; j < size; ++j) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left == NULL && node->right == NULL) return i;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+                
+            }
+        }
+        return i;
+    }
+};
+int main(void) {
+    TreeNode *n1 = new TreeNode(1);
+    Solution s;
+    cout << s.minDepth(n1) << endl;
+    return 0;
+}
