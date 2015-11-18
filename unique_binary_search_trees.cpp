9c41338a8417968027b9cf6876aa802a06ca38b7
Candice Yang <capricorn.young@gmail.com>
2015-11-18 12:13:33 -0800

    update

---
+++ unique_binary_search_trees.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int numTrees(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[n];
+    }
+};
+int main(void) {
+    Solution s;
+    cout << s.numTrees(10) << endl;
+    return 0;
+}
