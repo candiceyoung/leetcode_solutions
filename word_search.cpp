2aced4254bd8dc728cc8b0f9f3c0dd13dc4ffcf9
Candice Yang <capricorn.young@gmail.com>
2015-12-04 23:16:19 -0800

    word search

--- word_search.cpp
+++ word_search.cpp
@@ -5,33 +5,30 @@ using namespace std;
 class Solution {
 public:
 	bool dfs(vector<vector<char>>& board, int row, int col, string &word, int start, vector<vector<int>>& visited) {
-        cout << "row: " << row << "col: " << col << endl;
 		if (word[start] != board[row][col]) return false;
+        visited[row][col] = 1;
 		if (start == word.size() - 1) return true;
-		if (row > 0) {
-            cout << "try upward" << endl;
+		if (row > 0 && visited[row - 1][col] == 0) {
 			visited[row - 1][col]  = 1;
 			if (dfs(board, row - 1, col, word, start + 1, visited) == true) return true;
 			visited[row - 1][col] = 0;
 		}
-		if (col < board[0].size() - 1) {
-            cout << "try right" << endl;
+		if (col < board[0].size() - 1 && visited[row][col + 1] == 0) {
 			visited[row][col + 1] = 1;
 			if (dfs(board, row, col + 1, word, start + 1, visited) == true) return true;
 			visited[row][col + 1] = 0;
 		}
-		if (row < board.size() - 1) {
-            cout << "try downward" << endl;
+		if (row < board.size() - 1 && visited[row + 1][col] == 0) {
 			visited[row + 1][col] = 1;
 			if (dfs(board, row + 1, col, word, start + 1, visited) == true) return true;
 			visited[row + 1][col] = 0;
 		}
-		if (col > 0) {
-            cout << "try left" << endl;
+		if (col > 0 && visited[row][col - 1] == 0) {
 			visited[row][col - 1] = 1;
 			if(dfs(board, row, col - 1, word, start + 1, visited) == true) return true;
-			visited[row - 1][col] = 0;
+			visited[row][col - 1] = 0;
 		}
+        visited[row][col] = 0;
 		return false;
 	}
 	bool exist(vector<vector<char>>& board, string word) {
@@ -51,7 +48,12 @@ int main(void) {
 		{'S', 'F', 'C', 'S'},
 		{'A', 'D', 'E', 'E'}
 	};
-	string word = "SEE";
-	cout << s.exist(board, word) << endl;
+    vector<vector<char>> board1 = {
+        {'C', 'A', 'A'},
+        {'A', 'A', 'A'},
+        {'B', 'C', 'D'}
+    };
+	string word = "AAB";
+	cout << s.exist(board1, word) << endl;
 	return 0;
 }
