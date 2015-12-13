17f4aea3f353fa190fa1d478657ee38b725e32e8
Candice Yang <capricorn.young@gmail.com>
2015-12-13 10:42:31 -0800

    update

---
+++ N_Queens.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool check(vector<string> &temp, int row, int col) {
+        for (int i = 0; i < row; ++i) {
+            if (temp[i][col] == 'Q') return false;
+        }
+        int row_copy = row - 1, col_copy = col - 1;
+        while (row_copy >= 0 && col_copy >= 0) {
+            if (temp[row_copy][col_copy] == 'Q') return false;
+            row_copy--; col_copy--;
+        }
+        int row_copy1 = row - 1, col_copy1 = col + 1;
+        while (row_copy1 >= 0 && col_copy1 < temp[0].length()) {
+            if (temp[row_copy1][col_copy1] == 'Q') return false;
+            row_copy1--; col_copy1++;
+        }
+        return true;
+    }
+    void recursive(int n, vector<vector<string>> &result, int row, vector<string> &temp) {
+        if (row >= n) {
+            result.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (check(temp, row, i) == true) {
+                string row_str(n, '.');
+                row_str[i] = 'Q';
+                temp.push_back(row_str);
+                recursive(n, result, row+1, temp);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> result;
+        vector<string> temp;
+        recursive(n, result, 0, temp);
+        return result;
+    }
+};
+int main(void) {
+    Solution s;
+    vector<vector<string>> result = s.solveNQueens(4);
+    for (int i = 0; i < result.size(); ++i) {
+        for (int j = 0; j < result[i].size(); ++j) {
+            cout << result[i][j] << endl;
+        }
+    }
+    return 0;
+}
