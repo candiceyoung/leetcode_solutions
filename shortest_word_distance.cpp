e1a98efa8bc93a5b1f745ab97ee50ff638425989
Candice Yang <capricorn.young@gmail.com>
2015-12-25 15:15:21 -0800

    update

--- shortest_word_distance.cpp
+++ shortest_word_distance.cpp
@@ -2,22 +2,13 @@ class Solution {
 public:
     int shortestDistance(vector<string>& words, string word1, string word2) {
         int distance = INT_MAX;
-        for (int i = 0; i < words.size() - 1; ++i) {
-            if (words[i] == word1) {
-                for (int j = i + 1; j < words.size(); ++j) {
-                    if (words[j] == word2) {
-                        if (distance > (j - i)) distance = j - i;
-                        continue;
-                    }
-                }
-            }
-            else if (words[i] == word2) {
-                for (int j = i + 1; j < words.size(); ++j) {
-                    if (words[j] == word1) {
-                        if (distance > (j - i)) distance = j - i;
-                        continue;
-                    }
-                }
+        int pos1 = -1, pos2 = -1;
+        for (int i = 0; i < words.size(); ++i) {
+            if (words[i] == word1) pos1 = i;
+            else if (words[i] == word2) pos2 = i;
+            else continue;
+            if (pos1 != -1 && pos2 != -1) {
+                if (abs(pos1 - pos2) < distance) distance = abs(pos1 - pos2);
             }
         }
         return distance;
