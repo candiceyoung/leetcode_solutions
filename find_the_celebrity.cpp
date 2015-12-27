12655596223eed7fe8aba83f69950b7a44187e2c
Candice Yang <capricorn.young@gmail.com>
2015-12-26 16:24:27 -0800

    update

---
+++ find_the_celebrity.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Forward declaration of the knows API.
+bool knows(int a, int b) {
+    if (a == 0 && b == 1) return false;
+    else return true;
+}
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        vector<int> v;
+        for (int i = 0; i < n; ++i) {
+            int count = 0;
+            for (int j = 0; j < n; ++j) {
+                if (j == i) continue;
+                if (knows(j, i) == false) break;
+                else count++;
+            }
+            if (count == (n - 1)) v.push_back(i);
+        }
+        int size = v.size(); 
+        if (size == 0) return -1;
+        for (int i = 0; i < size; ++i) {
+            int flag = 0;
+            for (int j = 0; j < n; ++j) {
+                if (j == v[i]) continue;
+                if (knows(v[i], j)) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) return v[i];
+        }
+        return -1;
+    }
+};
+int main(void) {
+    Solution s;
+    cout << s.findCelebrity(2) << endl;
+    return 0;
+}
