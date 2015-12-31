45e7596dc351a60f78481028d37fd70b99b65f8a
Candice Yang <capricorn.young@gmail.com>
2015-12-30 23:45:26 -0800

    update

---
+++ closest_binary_search_tree_value.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        int a = root->val;
+        TreeNode *kid = target < a ? root->left : root->right;
+        if (!kid) return a;
+        int b = closestValue(kid, target);
+        return abs(a - target) < abs(b - target) ? a : b;
+    }
+};
