8c4e75b959e21eb775932fdd6d7771ee26c99447
Candice Yang <capricorn.young@gmail.com>
2016-01-09 20:35:14 -0800

    update

--- majority_element.cpp
+++ majority_element.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        int candidate, count = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == candidate || count == 0) {
+                candidate = nums[i];
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        count = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == candidate) count++;
+        }
+        if (count > nums.size() / 2) return candidate;
+        else return 0;
     }
 };
