0afbc28f2a9ac78b9f13245881e5c08c9d5b2cf3
Candice Yang <capricorn.young@gmail.com>
2016-01-14 17:48:45 -0800

    update

---
+++ power_of_three.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n < 1) return false;
+        if (n == pow(3, (int)(log10(n)/log10(3)))) return true;
+        return false;
+    }
+};
