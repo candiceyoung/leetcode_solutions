2c68fe6d50c0ca0940cefc64780f0178406bcc83
Candice Yang <capricorn.young@gmail.com>
2016-01-17 15:21:38 -0800

    update

--- number_of_islands.cpp
+++ number_of_islands.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/* Solution 1: DFS
 class Solution {
 public:
     bool dfs(vector<vector<char>>& grid, int row, int col) {
@@ -24,6 +25,46 @@ public:
             }
         }
         return num;
+    }
+};
+*/
+/* Solution 2: union-find */
+class Solution {
+private:
+    int total;
+public:
+    int find(vector<int> &roots, int index) {
+        if (roots[index] == -1) return index;
+        else return find(roots, roots[index]);
+    }
+    int uni(vector<int> &roots, int a, int b) {
+        int a_root = find(roots, a);
+        int b_root = find(roots, b);
+        if (a_root != b_root) {
+            roots[b_root] = a_root;
+            total--;
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        int row = grid.size();
+        if (row == 0) return count;
+        int col = grid[0].size();
+        vector<int> roots(row * col, -1);
+        total = row * col;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (grid[i][j] == '0') { total--; continue; }
+                int index = i * col + j;
+                if (i - 1 >= 0 && grid[i - 1][j] == '1') {
+                    uni(roots, (i - 1) * col + j, index);
+                }
+                if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+                    uni(roots, i * col + j - 1, index);
+                }
+            }
+        }
+        return total;
     }
 };
 int main(void) {
