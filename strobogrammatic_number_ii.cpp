e9363dab56ba3314fab392b9418af4398f3832e3
Candice Yang <capricorn.young@gmail.com>
2016-01-16 21:25:07 -0800

    update

--- strobogrammatic_number_ii.cpp
+++ strobogrammatic_number_ii.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    void recursive(vector<string> &result, vector<char> &nums1, vector<char> &nums2, string start, int n) {
+        if (n == 0) {
+            if (start.length() > 1 && start[0] == '0') return;
+            result.push_back(start);
+            return;
+        }
+        for (int i = 0; i < nums1.size(); ++i) {
+            recursive(result, nums1, nums2, nums1[i] + start + nums2[i], n - 2);
+        }
+    }
+    vector<string> findStrobogrammatic(int n) {
+        vector<char> nums1 = {'0', '1', '6', '8', '9'};
+        vector<char> nums2 = {'0', '1', '9', '8', '6'};
+        vector<string> result;
+        if (n % 2 == 0) {
+            recursive(result, nums1, nums2, "", n);
+        }
+        else {
+            recursive(result, nums1, nums2, "0", n - 1);
+            recursive(result, nums1, nums2, "1", n - 1);
+            recursive(result, nums1, nums2, "8", n - 1);
+        }
+        return result;
+    }
+};
+/*
+class Solution {
+public:
     vector<string> recursive(int n) {
         vector<string> result;
         if (n == 1) {
@@ -54,3 +82,4 @@ public:
         
     }
 };
+*/
