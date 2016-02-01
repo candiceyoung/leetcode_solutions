ad8fd15cd975c816359059cd5e8e7403a60d3db6
Candice Yang <capricorn.young@gmail.com>
2016-01-31 21:50:18 -0800

    update

--- find_minimum_in_rotated_sorted_array.cpp
+++ find_minimum_in_rotated_sorted_array.cpp
@@ -12,6 +12,22 @@ public:
         return result;
     }
 };
+/*
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        // write your code here
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            if (nums[left] < nums[right]) return nums[left];
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1;
+            else right = mid;
+        }
+        return nums[left];
+    }
+};
+*/
 int main(void) {
     vector<int> vec = {4, 5, 1, 2, 3};
     Solution s;
