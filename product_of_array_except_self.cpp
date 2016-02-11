c1b3d1460640fd7e7e467af6f9ef15a292c0633f
Candice Yang <capricorn.young@gmail.com>
2016-02-11 13:29:39 -0800

    update

--- product_of_array_except_self.cpp
+++ product_of_array_except_self.cpp
@@ -1,45 +1,21 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 class Solution {
-    public:
-        vector<int> productExceptSelf(vector<int>& nums) {
-            vector<int> forward;
-            vector<int> backward;
-            vector<int> result;
-            vector<int>::iterator it;
-            if (nums.size() == 0) {
-                result.push_back(0);
-                return result;
-            }
-            int temp = 1;
-            forward.push_back(temp);
-            for (it = nums.begin()+1; it < nums.end(); ++it) {
-                temp *= *(it - 1);
-                forward.push_back(temp);
-            }
-            temp = 1;
-            backward.push_back(temp);
-            for (it = nums.end() - 2; it >= nums.begin(); --it) {
-                temp *= *(it + 1);
-                backward.push_back(temp);
-            }
-            vector<int>::iterator begin, end;
-            for (begin = forward.begin(), end = backward.end() - 1; begin < forward.end(), end >= backward.begin(); ++begin, --end) {
-                result.push_back((*begin) * (*end));
-            }
-            return result;
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> result;
+        int size = nums.size();
+        if (size == 0) return result;
+        vector<int> forward, backward;
+        forward.push_back(1);
+        for (int i = 1; i < size; ++i) {
+            forward.push_back(nums[i - 1] * forward[i - 1]);
         }
-};
-int main(void) {
-    Solution s;
-    vector<int> vec = {0, 0};
-    vector<int> result;
-    vector<int>::iterator it;
-    result = s.productExceptSelf(vec);
-    for (it = result.begin(); it < result.end(); ++it) {
-        cout << *it << "    ";
+        backward.push_back(1);
+        for (int i = size - 2; i >= 0; --i) {
+            backward.push_back(nums[i + 1] * backward[size - 2 - i]);
+        }
+        for (int i = 0; i < size; ++i) {
+            result.push_back(forward[i] * backward[size - 1 - i]);
+        }
+        return result;
     }
-    cout << endl;
-    return 0;
-}
+};
