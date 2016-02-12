f19ccdba961eaaf3ff85856205c8d44d00cc5a2a
Candice Yang <capricorn.young@gmail.com>
2016-02-12 10:57:33 -0800

    update

--- implement_a_trie.cpp
+++ implement_a_trie.cpp
@@ -1,65 +1,58 @@
-#define ALPHABETIC_SIZE 26
-#define INDEX(c) (int(c-'a'))
-
 class TrieNode {
 public:
     // Initialize your data structure here.
+    vector<TrieNode*> children;
+    bool isEnd;
     TrieNode() {
-        children.resize(ALPHABETIC_SIZE, NULL);
-        count = 0;
+        children.resize(26, NULL);
+        isEnd = false;
     }
-    vector<TrieNode*> children; 
-    int count;
 };
 
 class Trie {
 public:
     Trie() {
         root = new TrieNode();
-        count = 0;
     }
 
     // Inserts a word into the trie.
     void insert(string word) {
-        TrieNode * cur = root;
-        for(int i=0, len=word.size(); i<len; i++){
-            int index = INDEX(word[i]);
-            if(cur->children[index]==NULL){
+        TrieNode *cur = root;
+        for (int i = 0; i < word.length(); ++i) {
+            int index = word[i] - 'a';
+            if (!cur->children[index]) {
                 cur->children[index] = new TrieNode();
             }
             cur = cur->children[index];
         }
-        cur->count = ++count; 
+        cur->isEnd = true;
     }
 
     // Returns if the word is in the trie.
     bool search(string word) {
-        TrieNode * cur = root;
-        for(int i=0, len=word.size(); i<len; i++){
-            int index = INDEX(word[i]);
-            if(cur->children[index]==NULL)
-                return false;
+        TrieNode *cur = root;
+        for (int i = 0; i < word.length(); ++i) {
+            int index = word[i] - 'a';
+            if (!cur->children[index]) return false;
             cur = cur->children[index];
         }
-        return (cur->count)!=0; 
+        return cur->isEnd;
     }
 
     // Returns if there is any word in the trie
     // that starts with the given prefix.
     bool startsWith(string prefix) {
-        TrieNode * cur = root;
-        for(int i=0, len=prefix.size(); i<len; i++){
-            int index = INDEX(prefix[i]);
-            if(cur->children[index]==NULL)
-                return false;
+        TrieNode *cur = root;
+        for (int i = 0; i < prefix.length(); ++i) {
+            int index = prefix[i] - 'a';
+            if (!cur->children[index]) return false;
             cur = cur->children[index];
         }
-        return true;     
+        return true;
     }
 
 private:
     TrieNode* root;
-    int count;
 };
 
 // Your Trie object will be instantiated and called as such:
