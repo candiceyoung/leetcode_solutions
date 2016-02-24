32bb3680c44094a9dda6af3834709821a832fcd9
Candice Yang <capricorn.young@gmail.com>
2016-02-23 21:47:19 -0800

    update

---
+++ simplify_path.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> stk;
+        string result;
+        int len = path.length();
+        if (len == 0) return result;
+        if (path[0] != '/') return result;
+        int pos = 1;
+        while (path.find('/', pos) != string::npos) {
+            string current = path.substr(pos, path.find('/', pos) - pos);
+            pos = path.find('/', pos);
+            pos++;
+            if (current == ".") continue;
+            else if (current == ".." && !stk.empty()) stk.pop();
+            else if (current == ".." && stk.empty()) continue;
+            else if (current.length() == 0) continue;
+            else stk.push(current);
+        }
+        if (pos <= len - 1) {
+            string current = path.substr(pos, len - pos);
+            if (current == ".");
+            else if (current == ".." && !stk.empty()) stk.pop();
+            else if (current == ".." && stk.empty());
+            else if (current.length() == 0);
+            else stk.push(current);
+        }
+        if (stk.empty()) return "/";
+        while (!stk.empty()) {
+            string temp = stk.top();
+            stk.pop();
+            result = "/" + temp + result;
+        }
+        return result;
+    }
+};
