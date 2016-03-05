2240a44373d028c787c5990824b16ded95b340ea
Candice Yang <capricorn.young@gmail.com>
2016-03-05 14:25:31 -0800

    update

--- contains_duplicate_ii.cpp
+++ contains_duplicate_ii.cpp
@@ -3,14 +3,12 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int size = nums.size();
         if (size == 0) return false;
-        unordered_map<int, vector<int>> mp;
+        unordered_map<int, int> mp;
         for (int i = 0; i < size; ++i) {
-            if (mp.find(nums[i]) == mp.end()) mp[nums[i]].push_back(i);
+            if (mp.find(nums[i]) == mp.end()) mp[nums[i]] = i;
             else {
-                for (vector<int>::iterator it = mp[nums[i]].begin(); it != mp[nums[i]].end(); ++it) {
-                    if (abs(*it - i) <= k) return true;
-                }
-                mp[nums[i]].push_back(i);
+                if (i - mp[nums[i]] <= k) return true;
+                else mp[nums[i]] = i;
             }
         }
         return false;
