bc6b560640d7ffcc72bae008b1b363229a049c1a
Candice Yang <capricorn.young@gmail.com>
2016-03-06 12:06:58 -0800

    update

--- best_time_to_buy_and_sell_stock_ii.cpp
+++ best_time_to_buy_and_sell_stock_ii.cpp
@@ -1,34 +1,21 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 class Solution {
-    public:
-        int maxProfit(vector<int>& prices) {
-            vector<int>::iterator it;
-            int maxProfit = 0;
-            int start, end;
-            int flag = 0;
-            for (it = prices.begin(); it < prices.end() - 1; ++it) {
-                if ((*it <= *(it + 1)) && (!flag)) {
-                    start = *it;
-                    flag = 1;
-                }
-                else if ((*it > *(it + 1)) && flag) {
-                    end = *it;
-                    flag = 0;
-                    maxProfit += (end - start);
-                }
+public:
+    int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        if (size == 0) return 0;
+        int max_profit = 0;
+        int start = prices[0], end = prices[0];
+        for (int i = 1; i < size; ++i) {
+            if (prices[i] < end) {
+                if (start < end) max_profit += (end - start);
+                start = prices[i];
+                end = prices[i];
             }
-            if (*it >= *(it - 1)) {
-                end = *it;
-                maxProfit += (end - start);
+            else {
+                end = prices[i];
             }
-            return maxProfit;
         }
+        if (end > start) max_profit += (end - start);
+        return max_profit;
+    }
 };
-int main(void) {
-    Solution s;
-    vector<int> ivec = {1, 9, 6, 9, 1, 7, 1, 1, 5, 9, 9, 9};
-    cout << s.maxProfit(ivec) << endl;
-    return 0;
-}
