d9b67bc9cc13ffa8159016d243c0e975ac2e25cb
Candice Yang <capricorn.young@gmail.com>
2016-03-06 10:01:04 -0800

    update

--- single_number_iii.cpp
+++ single_number_iii.cpp
@@ -1,84 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <map>
-using namespace std;
-class Solution {
-    public:
-        vector<int> SingleNumber(vector<int>& nums) {
-            vector<int> result;
-            map<int, int> kv;
-            vector<int>::iterator it;
-            for (it = nums.begin(); it < nums.end(); ++it) {
-                if (kv.find(*it) == kv.end()) {
-                    kv[*it] = 1;
-                }
-                else kv[*it] += 1;
-            }
-            map<int, int>::iterator map_it;
-            for (map_it = kv.begin(); map_it != kv.end(); ++map_it) {
-                if (map_it->second == 1) result.push_back(map_it->first);
-            }
-            return result;
-        }
-};
-/*
 class Solution {
 public:
-    vector<int> singleNumber(vector<int> &A) {
-        // write your code here
-        vector<int> result;
-        int size = A.size();
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> result(2, 0);
+        int size = nums.size();
         if (size == 0) return result;
-        set<int> st;
+        int diff = 0;
+        for (int i = 0; i < size; ++i) diff ^= nums[i];
+        diff = diff & (-diff);
         for (int i = 0; i < size; ++i) {
-            if (st.find(A[i]) == st.end()) st.insert(A[i]);
-            else st.erase(A[i]);
-        }
-        for (set<int>::iterator it = st.begin(); it != st.end(); ++it) {
-            result.push_back(*it);
+            if (diff & nums[i]) result[0] ^= nums[i];
+            else result[1] ^= nums[i];
         }
         return result;
     }
 };
-*/
-/*
-class Solution
-{
-public:
-    vector<int> singleNumber(vector<int>& nums) 
-    {
-        // Pass 1 : 
-        // Get the XOR of the two numbers we need to find
-        int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
-        // Get its last set bit
-        diff &= -diff;
-
-        // Pass 2 :
-        vector<int> rets = {0, 0}; // this vector stores the two numbers we will return
-        for (int num : nums)
-        {
-            if ((num & diff) == 0) // the bit is not set
-            {
-                rets[0] ^= num;
-            }
-            else // the bit is set
-            {
-                rets[1] ^= num;
-            }
-        }
-        return rets;
-    }
-};
-*/
-int main(void) {
-    Solution s;
-    vector<int> vec = {1, 2, 1, 3, 2, 5};
-    vector<int> result;
-    result = s.SingleNumber(vec);
-    vector<int>::iterator it;
-    for (it = result.begin(); it < result.end(); ++it) {
-        cout << *it << "    ";
-    }
-    cout << endl;
-    return 0;
-}
