0aee978557339baf1dc9f9822427d39a98906caa
Candice Yang <capricorn.young@gmail.com>
2016-03-06 15:25:03 -0800

    update

--- generalized_abbreviation.cpp
+++ generalized_abbreviation.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void recursive(string &word, int start, vector<string> &result) {
-        int len = word.length();
+    void recursive(vector<string> &result, int start, string &word) {
         result.push_back(word);
-        for (int i = start; i < len; ++i) {
-            if ((i - 1) >= 0 && word[i - 1] >= '0' && word[i - 1] <= '9') continue;
-            for (int j = 1; j <= len - start; ++j) {
-                if ((len - i) >= j) {
-                    string temp = word;
-                    word = word.substr(0, i) + to_string(j) + word.substr(j + i, len - j - i);
-                    recursive(word, i + 1, result);
-                    word = temp;
-                }
+        for (int i = start; i < word.length(); ++i) {
+            if (i - 1 >= 0 && word[i - 1] >= '0' && word[i - 1] <= '9') continue;
+            for (int j = 1; j <= word.length() - i; ++j) {
+                string temp = word;
+                word = word.substr(0, i) + to_string(j) + word.substr(j + i, word.length() - i - j);
+                recursive(result, 1 + i, word);
+                word = temp;
             }
         }
     }
     vector<string> generateAbbreviations(string word) {
         vector<string> result;
         int len = word.length();
-        if (len == 0) { result.push_back(""); return result; }
-        recursive(word, 0, result);
-        cout << result.size() << endl;
+        if (len == 0) {
+            result.push_back("");
+            return result;
+        }
+        recursive(result, 0, word);
         return result;
     }
 };
