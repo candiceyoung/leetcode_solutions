35fac91f195bdc544fea926b674cfb38eaec99b1
Candice Yang <capricorn.young@gmail.com>
2016-03-06 16:42:45 -0800

    update

--- 3sum_smaller.cpp
+++ 3sum_smaller.cpp
@@ -1,35 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
 class Solution {
 public:
     int threeSumSmaller(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int count = 0;
         int size = nums.size();
-        if (size == 0 || size == 1 || size == 2) return 0;
-        int i = 0;
-        while (i < nums.size() - 2) {
-            int p1 = i+1, p2 = size - 1;
-            int t = target - nums[i];
+        if (size < 3) return 0;
+        int total = 0;
+        for (int i = 0; i < size - 2; ++i) {
+            int p1 = i + 1, p2 = size - 1;
             while (p1 < p2) {
-                if (nums[p1] + nums[p2] < t) {
-                    count = count + p2 - p1;
+                if (nums[i] + nums[p1] + nums[p2] < target) {
+                    total += p2 - p1;
                     p1++;
-                }
-                else {
-                    p2--;
-                }
+                } 
+                else p2--;
             }
-            i++;
         }
-        return count;
+        return total;
     }
 };
-int main(void) {
-    Solution s;
-    vector<int> nums = {-2, -2, 0, 1, 3};
-    cout << s.threeSumSmaller(nums, 2) << endl;
-    return 0;
-}
