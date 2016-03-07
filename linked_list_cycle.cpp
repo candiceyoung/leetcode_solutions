aacd93cf6bf429d0894590cb992d18ee1b4f4243
Candice Yang <capricorn.young@gmail.com>
2016-03-06 18:19:27 -0800

    update

--- linked_list_cycle.cpp
+++ linked_list_cycle.cpp
@@ -8,13 +8,12 @@ struct ListNode {
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head == NULL || head->next == NULL) return false;
-        ListNode *fast, *slow;
-        fast = head; slow = head;
-        while (fast->next != NULL && fast->next->next != NULL && slow->next != NULL) {
+        if (!head) return false;
+        ListNode *fast = head, *slow = head;
+        while (fast && fast->next && slow) {
             fast = fast->next->next;
             slow = slow->next;
-            if (fast == slow) return true;
+            if (slow && fast == slow) return true;
         }
         return false;
     }
