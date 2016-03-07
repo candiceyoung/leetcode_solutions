6c1f60d84c7177361c26ae96bac5c1773ea4297a
Candice Yang <capricorn.young@gmail.com>
2016-03-06 17:47:10 -0800

    update

--- single_number_ii.cpp
+++ single_number_ii.cpp
@@ -1,30 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <map>
-using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> counter;
-        vector<int>::iterator it;
-        for (it = nums.begin(); it < nums.end(); ++it) {
-            if (counter.find(*it) != counter.end()) {
-                counter[*it]++;
-            }
-            else counter[*it] = 1;
-        }
-        map<int, int>::iterator mit;
-        for (mit = counter.begin(); mit != counter.end(); ++mit) {
-            if ((*mit).second != 3) return (*mit).first;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) mp[nums[i]]++;
+        for (unordered_map<int, int>::iterator it = mp.begin(); it != mp.end(); ++it) {
+            if (it->second != 3) return it->first;
         }
         return 0;
     }
 };
-int main(void) {
-    Solution s;
-    vector<int> vec = {1, 2, 1, 3, 1, 3, 2, 2};
-    cout << s.singleNumber(vec) << endl;
-    vector<int> vec1 = {1, 1};
-    cout << s.singleNumber(vec1) << endl;
-    return 0;
-}
