5b8ef147bd7acda238f2b99b08afee9faab85ff8
candiceyoung <capricorn.young@gmail.com>
2016-03-11 15:37:13 -0800

    update

--- shortest_word_distance_ii.cpp
+++ shortest_word_distance_ii.cpp
@@ -1,21 +1,18 @@
 class WordDistance {
 private:
-    unordered_map<string, vector<int>> m;
+    unordered_map<string, vector<int>> mp;
 public:
     WordDistance(vector<string>& words) {
         for (int i = 0; i < words.size(); ++i) {
-            m[words[i]].push_back(i);
+            mp[words[i]].push_back(i);
         }
     }
 
     int shortest(string word1, string word2) {
-        vector<int> v1 = m[word1];
-        vector<int> v2 = m[word2];
-        int distance = INT_MAX;
-        for (int i = 0; i < v1.size(); ++i) {
-            for (int j = 0; j < v2.size(); ++j) {
-                if (abs(v1[i] - v2[j]) < distance) distance = abs(v1[i] - v2[j]);
-            }
+        int i = 0, j = 0, distance = INT_MAX;
+        while (i < mp[word1].size() && j < mp[word2].size()) {
+            distance = min(distance, abs(mp[word1][i] - mp[word2][j]));
+            mp[word1][i] < mp[word2][j] ? i++ : j++;
         }
         return distance;
     }
