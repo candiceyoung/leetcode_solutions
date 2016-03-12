ec947e2f1bee5541ff85c468e4f45738423b837c
candiceyoung <capricorn.young@gmail.com>
2016-03-11 16:57:14 -0800

    update

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -21,3 +21,35 @@ public:
         }
     }
 };
+
+/*
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int size = nums.size();
+        if (size == 0) return;
+        int p0 = 0, p2 = size - 1;
+        int i = 0;
+        while (p0 < p2 && i <= p2) {
+            if (nums[i] == 1) {
+                i++;
+                continue;
+            }
+            else if (nums[i] == 0) {
+                if (i >= p0) {
+                    swap(nums[i], nums[p0]);
+                    p0++;
+                }
+                else i++;
+            }
+            else if (nums[i] == 2) {
+                if (i <= p2) {
+                    swap(nums[i], nums[p2]);
+                    p2--;
+                }
+                else i--;
+            }
+        }
+    }
+};
+*/
