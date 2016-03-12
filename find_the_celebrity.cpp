1ca3b5c33909efcf3d03a6fc8a58f3c74d4f6f86
candiceyoung <capricorn.young@gmail.com>
2016-03-11 16:03:48 -0800

    update

--- find_the_celebrity.cpp
+++ find_the_celebrity.cpp
@@ -42,3 +42,26 @@ int main(void) {
     cout << s.findCelebrity(2) << endl;
     return 0;
 }
+
+
+/*
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        if (n <= 1) return -1;
+        int candidate = 0;
+        for (int i = 1; i < n; ++i) {
+            if (!knows(i, candidate)) candidate = i;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (i == candidate) continue;
+            if (!knows(i, candidate) || knows(candidate, i)) return -1;
+        }
+        return candidate;
+    }
+};
+
+*/
