7c40a21fcf0082d7905f6d7c810bc714840a2b77
candiceyoung <capricorn.young@gmail.com>
2016-03-12 16:41:59 -0800

    update

--- set_matrix_zeroes.cpp
+++ set_matrix_zeroes.cpp
@@ -1,38 +1,29 @@
-// O(1) space: store the state in the first element in the row and col
-#include <iostream>
-#include <vector>
-#include <set>
-using namespace std;
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int> col;
-        set<int> row;
-        set<int>::iterator it;
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
+        int row = matrix.size();
+        if (row == 0) return;
+        int col = matrix[0].size();
+        int col0 = 1;
+        for (int i = 0; i < row; ++i) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < col; ++j) {
                 if (matrix[i][j] == 0) {
-                    if (row.find(i) == row.end()) row.insert(i);
-                    if (col.find(j) == col.end()) col.insert(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (it = row.begin(); it != row.end(); ++it) {
-            for (int i = 0; i < matrix[0].size(); ++i) {
-                matrix[*it][i] = 0;
+        for (int i = 1; i < row; ++i) {
+            for (int j = 1; j < col; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
-        for (it = col.begin(); it != col.end(); ++it) {
-            for (int i = 0; i < matrix.size(); ++i) {
-                matrix[i][*it] = 0;
-            }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < col; ++i) matrix[0][i] = 0;
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < row; ++i) matrix[i][0] = 0;
         }
     }
 };
-int main(void) {
-    vector<vector<int>> matrix = {{5, 8, 3, 0, 1, 4}, {2, 0, 6, 1, 1, 1}, {2, 3, 4, 5, 6, 6}};
-    Solution s;
-    s.setZeroes(matrix);
-    cout << matrix[0][0] << endl;
-    return 0;
-}
