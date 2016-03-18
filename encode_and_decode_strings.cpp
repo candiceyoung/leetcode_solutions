32fd889b02694d73c5d15a105a729ae24621b62e
candiceyoung <capricorn.young@gmail.com>
2016-03-18 12:49:48 -0700

    update

--- encode_and_decode_strings.cpp
+++ encode_and_decode_strings.cpp
@@ -7,37 +7,31 @@ public:
 
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
-        string encoded_string;
+        string encoded_string = "";
         int size = strs.size();
         if (size == 0) return encoded_string;
         for (int i = 0; i < size; ++i) {
-            int len = strs[i].length();
-            encoded_string = encoded_string + "*" + to_string(len) + "*" + strs[i];
+            encoded_string += "*" + to_string(strs[i].length()) + "*" + strs[i];
         }
         return encoded_string;
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
-        vector<string> strs;
+        vector<string> result;
         int len = s.length();
-        if (len == 0) return strs;
-        string temp = "";
+        if (len == 0) return result;
         int i = 1;
-        int start = 0;
         while (i < len) {
             int l = 0, j;
             for (j = i; s[j] != '*' && s[j] >= '0' && s[j] <= '9'; ++j) {
                 l = l * 10 + s[j] - '0';
             }
-            if (start == 1) strs.push_back(temp);
-            temp = "";
-            temp = s.substr(j + 1, l);
+            string temp = s.substr(j + 1, l);
             i = j + l + 2;
-            start = 1;
+            result.push_back(temp);
         }
-        strs.push_back(temp);
-        return strs;
+        return result;
     }
 };
 
