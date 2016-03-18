d920fa13013b5bb8285661a316409dcde4fb29dc
candiceyoung <capricorn.young@gmail.com>
2016-03-18 15:17:50 -0700

    update

--- minimum_size_subarray_sum.cpp
+++ minimum_size_subarray_sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-        int head = 0;
         int size = nums.size();
         if (size == 0) return 0;
+        int left = 0, right = 0;
         int minimum = INT_MAX;
         int sum = 0;
-        for (int i = 0; i < size; ++i) {
-            sum += nums[i];
+        while (right < size && left <= right) {
+            sum += nums[right];
             while (sum >= s) {
-                minimum = min(minimum, i - head + 1);
-                sum = sum - nums[head];
-                head++;
+                minimum = min(minimum, right - left + 1);
+                sum -= nums[left++];
             }
+            right++;
         }
         if (minimum == INT_MAX) return 0;
-        else return minimum;
+        return minimum;
     }
 };
