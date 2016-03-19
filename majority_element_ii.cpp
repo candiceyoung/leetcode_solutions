c8dcde27e706e7207c387a641b52d1e938e6ae67
candiceyoung <capricorn.young@gmail.com>
2016-03-18 19:05:57 -0700

    update

--- majority_element_ii.cpp
+++ majority_element_ii.cpp
@@ -1,18 +1,22 @@
+#include <vector>
+#include <iostream>
+using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int candidate1, candidate2;
         int count1 = 0, count2 = 0;
         for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == candidate1 || count1 == 0) {
+            
+            if (nums[i] != candidate2 && (nums[i] == candidate1 || count1 == 0)) {
                 candidate1 = nums[i];
                 count1++;
             }
-            else if (nums[i] == candidate2 || count2 == 0) {
+            else if (nums[i] != candidate1 && (nums[i] == candidate2 || count2 == 0)) {
                 candidate2 = nums[i];
                 count2++;
             }
-            else if (nums[i] != candidate1 || nums[i] != candidate2) {
+            else if (nums[i] != candidate1 && nums[i] != candidate2) {
                 count1--;
                 count2--;
             }
@@ -28,3 +32,11 @@ public:
         return result;
     }
 };
+int main(void) {
+    Solution s;
+    vector<int> nums = {1, 2, 2, 3, 2, 1, 1, 3};
+    vector<int> result;
+    result = s.majorityElement(nums);
+    cout << result[0] << "  " << result[1] << endl;
+    return 0;
+}
