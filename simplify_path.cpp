f84739fc359360e682a0681ebe4ef02f4ec7e949
candiceyoung <capricorn.young@gmail.com>
2016-03-19 16:35:49 -0700

    update

--- simplify_path.cpp
+++ simplify_path.cpp
@@ -34,3 +34,34 @@ public:
         return result;
     }
 };
+/*
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> stk;
+        string result;
+        int len = path.length();
+        if (len == 0) return result;
+        if (path[0] != '/') return result;
+        int pos = 1;
+        path += '/';
+        while (path.find('/', pos) != string::npos) {
+            string current = path.substr(pos, path.find('/', pos) - pos);
+            pos = path.find('/', pos);
+            pos++;
+            if (current == ".") continue;
+            else if (current == ".." && !stk.empty()) stk.pop();
+            else if (current == ".." && stk.empty()) continue;
+            else if (current.length() == 0) continue;
+            else stk.push(current);
+        }
+        if (stk.empty()) return "/";
+        while (!stk.empty()) {
+            string temp = stk.top();
+            stk.pop();
+            result = "/" + temp + result;
+        }
+        return result;
+    }
+};
+*/
