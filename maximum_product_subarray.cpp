2ff6e9635f2b07b0c6ef8d49bdd58418dc68feba
candiceyoung <capricorn.young@gmail.com>
2016-03-19 15:40:14 -0700

    update

--- maximum_product_subarray.cpp
+++ maximum_product_subarray.cpp
@@ -31,3 +31,34 @@ int main(void) {
     cout << s.maxProduct(nums) << endl;
     return 0;
 }
+
+/*
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int size = nums.size();
+        if (size == 0) return 0;
+        vector<int> positive(size);
+        vector<int> negative(size);
+        int maximum = nums[0];
+        positive[0] = nums[0];
+        negative[0] = nums[0];
+        for (int i = 1; i < size; ++i) {
+            if (nums[i] > 0) {
+                positive[i] = (positive[i - 1] > 0) ? positive[i - 1] * nums[i] : nums[i];
+                negative[i] = (negative[i - 1] < 0) ? negative[i - 1] * nums[i] : nums[i];
+            }
+            else if (nums[i] < 0) {
+                positive[i] = (negative[i - 1] < 0) ? negative[i - 1] * nums[i] : nums[i];
+                negative[i] = (positive[i - 1] > 0) ? positive[i - 1] * nums[i] : nums[i];
+            }
+            else {
+                positive[i] = 0;
+                negative[i] = 0;
+            }
+            maximum = max(maximum, positive[i]);
+        }
+        return maximum;
+    }
+};
+*/
