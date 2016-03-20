8186429673ebb0922b6065bf85ed8b485edc5751
candiceyoung <capricorn.young@gmail.com>
2016-03-20 16:43:32 -0700

    update

--- permutations_ii.cpp
+++ permutations_ii.cpp
@@ -42,3 +42,28 @@ int main(void) {
     }
     return 0;
 }
+
+
+
+/*
+class Solution {
+public:
+    void recursion(vector<int> num, int i, vector<vector<int>> &res) {
+        if (i >= num.size()) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < num.size(); k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, res);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int>>res;
+        recursion(num, 0, res);
+        return res;
+    }
+};
+*/
