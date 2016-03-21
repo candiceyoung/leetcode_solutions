a44242598dff4e03c65e7a443d1ced9a46d13df7
candiceyoung <capricorn.young@gmail.com>
2016-03-20 20:27:44 -0700

    update

--- trapping_rain_water.cpp
+++ trapping_rain_water.cpp
@@ -28,3 +28,29 @@ int main(void) {
     cout << s.trap(height) << endl;
     return 0;
 }
+
+/*
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int size = height.size();
+        if (size == 0) return 0;
+        int left = 0, right = size - 1;
+        int maximum = 0;
+        int left_max = 0, right_max = 0;
+        while (left <= right) {
+            left_max = max((int)height[left], left_max);
+            right_max = max((int)height[right], right_max);
+            if (left_max < right_max) {
+                maximum += (left_max - height[left]);
+                left++;
+            }
+            else {
+                maximum += (right_max - height[right]);
+                right--;
+            }
+        }
+        return maximum;
+    }
+};
+*/
